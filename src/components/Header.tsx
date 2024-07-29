6aac9656db8c54fa951f9fb7ca0987759dca19f8
Gabriel <victorgabriel1730@gmail.com>
2024-07-29 08:55:10 -0300

    style: animation from navbar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { MotionConfig } from "framer-motion";
+import { MotionConfig, Variants } from "framer-motion";
 import Hamburger from "hamburger-react";
 import Image from "next/image";
 import Link from "next/link";
@@ -18,25 +18,29 @@ const Header = () => {
   function toggleNav() {
     showNav ? setShowNav(false) : setShowNav(true);
   }
+  const variants: Variants = {
+    open: { opacity: 1, x: 0, y: 0, display: "block" },
+    closed: { opacity: 0, x: 100, display: "none" },
+  };
+
   return (
     <header className="p-5 relative">
       <div className="flex justify-between items-center text-white-standard">
         <Image src="/logo.svg" width={150} height={100} alt="Logo pessoal" />
         <Hamburger rounded size={25} onToggle={toggleNav} toggled={showNav} />
       </div>
-      {showNav && (
-        <motion.div initial={{ x: -100 }}>
-          <nav className="bg-background absolute top-[85px] w-full h-screen left-0 py-5">
-            <ul className="flex flex-col items-center font-inter text-lg font-normal">
-              {navLinks.map((link, index) => (
-                <li key={index} className="p-2">
-                  <Link href={link.route}>{link.name}</Link>
-                </li>
-              ))}
-            </ul>
-          </nav>
-        </motion.div>
-      )}
+
+      <motion.div variants={variants} animate={showNav ? "open" : "closed"}>
+        <nav className="bg-background absolute w-full h-screen left-0 py-5">
+          <ul className="flex flex-col items-center font-inter text-lg font-normal w-full">
+            {navLinks.map((link, index) => (
+              <li key={index} className="p-2">
+                <Link href={link.route}>{link.name}</Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </motion.div>
     </header>
   );
 };
